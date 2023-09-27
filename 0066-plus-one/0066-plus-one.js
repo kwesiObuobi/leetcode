adc3275f11e9baa981a4e4b47f2961ce04a9f191
Samuel Obuobi Lartey <kwesiobuobi7@gmail.com>
2023-09-27 15:38:37 +0000

    Time: 48 ms (77.42%), Space: 42 MB (42.82%) - LeetHub

--- 0066-plus-one/0066-plus-one.js
+++ 0066-plus-one/0066-plus-one.js
@@ -3,22 +3,27 @@
  * @return {number[]}
  */
 var plusOne = function(digits) {
-  if (digits[digits.length - 1] + 1 !== 10) {
-      digits[digits.length-1] = digits[digits.length-1] + 1;
-      return digits;
-  }
-    
-  let i = 1;
-  while(digits[digits.length - i] + 1 === 10) {
-    digits[digits.length - i] = 0;
-    i++;
-  }
-
-  if (i > digits.length) {
-    digits.unshift(1)
-  } else {
-    digits[digits.length-i] = digits[digits.length-i] + 1;
-  }
-
+  let digitsStr = digits.join('');
+  digitsStr = (BigInt(digitsStr) + 1n).toString();
+  digits = digitsStr.split('').map(Number) 
   return digits;
+
+  // if (digits[digits.length - 1] + 1 !== 10) {
+  //     digits[digits.length-1] = digits[digits.length-1] + 1;
+  //     return digits;
+  // }
+    
+  // let i = 1;
+  // while(digits[digits.length - i] + 1 === 10) {
+  //   digits[digits.length - i] = 0;
+  //   i++;
+  // }
+
+  // if (i > digits.length) {
+  //   digits.unshift(1)
+  // } else {
+  //   digits[digits.length-i] = digits[digits.length-i] + 1;
+  // }
+
+  // return digits;
 };
