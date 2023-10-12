51c70c750976bac77f21a8fdc648bf9a775d51d3
Samuel Obuobi Lartey <kwesiobuobi7@gmail.com>
2023-10-12 15:29:33 +0000

    Time: 50 ms (62.41%), Space: 41.6 MB (85.88%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.js
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -3,5 +3,17 @@
  * @return {number}
  */
 var lengthOfLastWord = function(s) {
-    return s.trim().split(' ')[s.trim().split(' ').length - 1].length;
+    // return s.trim().split(' ')[s.trim().split(' ').length - 1].length;
+
+    let i  = s.length-1;
+    while(i >= 0 && s[i] === ' ') {
+      i--;
+    }
+
+    let count = 0;
+    while (i >= 0 && s[i] !== ' ') {
+      i--;
+      count++;
+    }
+    return count;
 };
